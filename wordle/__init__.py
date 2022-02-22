9f7a2aa609242ca02b0befc2a8a852bea7023cdb
Randy J. Ray <rjray@blackperl.com>
2022-02-21 20:20:39 -0800

    Updates

--- wordle/__init__.py
+++ wordle/__init__.py
@@ -5,6 +5,8 @@ This package contains:
 game: The class that plays the game from the Wordle side, to be callable by the
       agent.
 
+utils: Common code/utility functions.
+
 """
 
 __version__ = '1.0.0'
