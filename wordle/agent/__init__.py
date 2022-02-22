0f76e882aa589ede9a6492ac263746a56602a7c0
Randy J. Ray <rjray@blackperl.com>
2022-02-21 20:19:12 -0800

    Python meta-file for this directory

---
+++ wordle/agent/__init__.py
@@ -0,0 +1,13 @@
+"""CS 5033: Machine Learning Reinforcement Learning Project
+
+This package contains:
+
+base: A base class for the different agent implementations to use
+
+simple: A "simple" agent that uses little or no learning, as a baseline
+
+"""
+
+__version__ = '1.0.0'
+__all__ = []
+__author__ = 'Randy J. Ray <rjray@ou.edu>'
