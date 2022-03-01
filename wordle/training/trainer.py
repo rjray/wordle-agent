9da9d89fe3d7e62281775e12b9b67ab3f88ef84d
Randy J. Ray <rjray@blackperl.com>
2022-03-01 12:12:44 -0800

    Checkpoint (TGLP table)

--- wordle/training/trainer.py
+++ wordle/training/trainer.py
@@ -16,12 +16,15 @@ class Trainer():
         self.game = game
         self.base = base
 
+    def local_file(self, file):
+        return file if os.path.isabs(file) else os.path.join(self.base, file)
+
     def create_letter_pos_probabilities(self, file):
         answers = self.game.answers
         ans_count = len(answers)
         base = ord("a")
         counters = [Counter() for _ in range(5)]
-        fname = file if os.path.isabs(file) else os.path.join(self.base, file)
+        fname = self.local_file(file)
 
         for word in answers:
             for i, c in enumerate(word):
@@ -36,3 +39,37 @@ class Trainer():
             json.dump(probabilities, f, indent=2)
 
         return
+
+    def load_letter_pos_probabilities(self, file):
+        fname = self.local_file(file)
+
+        with open(fname, "r") as f:
+            data = json.load(f)
+
+        return data
+
+
+    def create_tglp_table(self, green_probabilities, file):
+        tglp = {}
+        base = ord("a")
+        fname = self.local_file(file)
+
+        for word in self.game.words:
+            total = 0.0
+            for i, ch in enumerate(word):
+                total += green_probabilities[ord(ch) - base][i]
+
+            tglp[word] = total
+
+        with open(fname, "w") as f:
+            json.dump(tglp, f, indent=2)
+
+        return
+
+    def load_tglp_probabilities(self, file):
+        fname = self.local_file(file)
+
+        with open(fname, "r") as f:
+            data = json.load(f)
+
+        return data
