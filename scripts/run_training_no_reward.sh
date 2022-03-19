cf86cb1a0254063854ed1b28205aa6a403cd9a23
Randy J. Ray <rjray@blackperl.com>
2022-03-18 17:40:20 -0700

    More scripts (training and comparison runs)

---
+++ scripts/run_training_no_reward.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+now=$(date '+%s')
+
+./train_agents.py \
+    --max 6 \
+    --agent sarsa \
+    --agent qlearning \
+    --game-arguments randomize=True,seed=$now \
+    --spread \
+    --runs 50 \
+    --reward 0 \
+    --output agent_training-0.90.csv
+
+./train_agents.py \
+    --max 6 \
+    --agent sarsa \
+    --agent qlearning \
+    --game-arguments randomize=True,seed=$now \
+    --spread \
+    --runs 50 \
+    --reward 0 \
+    --gamma 0.75 \
+    --output agent_training-0.75.csv
